7597d8772925e9cab166c591c44ac5276ece3c3c
tszandy <wxie@umass.edu>
2016-11-13 15:10:44 -0500

    fix build search.html add search*.js

---
+++ app/search/search.js
@@ -0,0 +1,18 @@
+import React from 'react';
+export default class Search extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {}
+  }
+  render(){
+    return(
+      <div>
+        <Navbar search="active" user={this.state}/>
+        
+
+
+      </div>
+    )
+  }
+
+}
