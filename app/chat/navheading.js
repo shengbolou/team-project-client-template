1a8e8deb8a305b47170deb7583fbd61eac95e19c
kimonuduo <lyc0128@gmail.com>
2016-11-12 21:39:11 -0500

    update chat

---
+++ app/chat/navheading.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default class NavHeading extends React.Component{
+
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return(
+
+                      <div className="panel-heading panel-heading-chat">
+                          <ul className="nav nav-pills nav-justified">
+                              <li role="presentation" className={this.props.chat}>
+                                <a>Chats</a>
+                              </li>
+                              <li role="presentation" className={this.props.friends}>
+                                  <a href="#">Friends</a>
+                              </li>
+                          </ul>
+                      </div>
+
+    )
+  }
+}
