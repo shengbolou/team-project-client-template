f46189ebaf81d3fcb96bd3d10b6832312c3bfa6e
shengbolou <loushengbo@hotmail.com>
2016-12-02 10:42:29 -0500

    chat update'

--- client/app/chat/navchatitem.js
+++ client/app/chat/navchatitem.js
@@ -1,5 +1,6 @@
 import React from 'React';
-import {Link} from 'react-router'
+import {Link} from 'react-router';
+
 export default class NavChatItem extends React.Component {
     constructor(props) {
         super(props);
